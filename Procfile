c0bb1825271d61992d89fee35d8583b07a355349
Michael Shea <mjshea3@illinois.edu>
2019-02-13 20:36:56 -0600

    some heroku stuff

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn StudySpaceUIUC.wsgi --log-file -
--- StudySpaceUIUC/settings.py
+++ StudySpaceUIUC/settings.py
@@ -118,3 +118,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
