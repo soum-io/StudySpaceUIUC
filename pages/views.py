307d18bd0bf5100fbb820665f5f0bf6e868ca6ff
Michael Shea <mjshea3@illinois.edu>
2019-03-04 18:43:25 -0600

    added google places and javascript api, and did minor bug fixes for results page

--- pages/views.py
+++ pages/views.py
@@ -3,10 +3,6 @@ from django.shortcuts import render
 
 
 # Create your views here.
-def home_view(request, *args, **kwargs):
-    return HttpResponse("<h1>Home</h1>")
-
-
 def login_view(request, *args, **kwargs):
     return render(request, "login/login.html", {})
 
