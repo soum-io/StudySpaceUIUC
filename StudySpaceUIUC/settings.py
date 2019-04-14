7bbec232b4936b349b66e8fee3f4416a72eb8b54
Michael Shea <mjshea3@illinois.edu>
2019-04-14 12:08:47 -0500

    hopefully fix static images issue

--- StudySpaceUIUC/settings.py
+++ StudySpaceUIUC/settings.py
@@ -16,7 +16,7 @@ import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
@@ -134,6 +134,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 
-STATICFILES_DIRS = ([os.path.join(BASE_DIR, "static")])
+# STATICFILES_DIRS = ([os.path.join(BASE_DIR, "static")])
