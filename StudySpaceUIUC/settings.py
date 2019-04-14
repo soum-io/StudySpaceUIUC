115bf54b19dd28720b8ae3029cd2bbbb9ada665d
Michael Shea <mjshea3@illinois.edu>
2019-04-14 12:19:52 -0500

    changed folder structure to work with cloud

--- StudySpaceUIUC/settings.py
+++ StudySpaceUIUC/settings.py
@@ -16,7 +16,15 @@ import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/2.1/howto/static-files/
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = ([os.path.join(BASE_DIR, "staticfiles")])
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
@@ -129,11 +137,3 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.1/howto/static-files/
-
-# STATIC_URL = '/static/'
-
-# STATICFILES_DIRS = ([os.path.join(BASE_DIR, "static")])
