38e822173b6a5c61a4c9d7152dcce360d6523341
Akhil Chainani <akhil.chainani1@gmail.com>
2019-04-18 23:42:22 -0500

    hours of operation delete bug fixed

--- load_data.py
+++ load_data.py
@@ -3,6 +3,9 @@ from pages.models import recordData
 
 print("Hello")
 df = pd.read_csv("~/Downloads/ALLRecords.csv")
+for record in recordData.objects.all():
+    print(record)
+    record.delete()
 for index, entry in df.iterrows():
     print(entry)
     temp_entry = recordData(libName=entry["libName"], dayOfWeek=entry["dayOfWeek"], time=entry["time"], floorNum=entry["floorNum"], section=entry["section"], count=entry["count"])
